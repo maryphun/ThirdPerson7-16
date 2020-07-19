930226f92f73b0b75238362cb7afd6dbe5a726e8
Mary <phunmung@gmail.com>
2020-07-20 03:56:51 +0900

    third day

--- Assets/ThirdPersonControl.cs
+++ Assets/ThirdPersonControl.cs
@@ -2,65 +2,109 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Cinemachine;
 
 public class ThirdPersonControl : MonoBehaviour
 {
-    public Transform camera;
+    public Camera camera;
     public Rigidbody rigidbody;
     public Animator animator;
     public LayerMask layerMask;
+    public Transform lockOnTarget;
+    public CinemachineVirtualCamera lockOnCamera;
+    public CinemachineFreeLook freeLookCamera;
+    public GameObject Reticle;
 
     public float speed = 0.001f;
     public float frontSpeedMultiplier = 1.25f;
+    public float horizontalSpeedMultiplier = 1.25f;
     public float animationTransmitionRate = 5.0f;
     public float turnSmoothTime = 0.01f;
     public float turnSmoothVelocity;
+    public float lockOnSearchRange = 20.0f;
+    public float climbHeightMaximum = 0.25f;
+    public float YaxisClimbLerpDelay = 3;   //The higher the value the smoothier it is but take more memory
 
     float horizontal;
     float vertical;
     float moveSpeed;    //speed after multiplier this frame
+    bool lockOnMode = false;
+
+    void Start()
+    {
+        //initialization
+        lockOnCamera.Priority = 0;
+        freeLookCamera.Priority = 1;
+        lockOnMode = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
         moveSpeed = speed; // set to default
 
+        //input
         horizontal = Mathf.MoveTowards(horizontal, Input.GetAxisRaw("Horizontal") * 100f, animationTransmitionRate);
         vertical = Mathf.MoveTowards(vertical, Input.GetAxisRaw("Vertical") * 100f, animationTransmitionRate);
+        if (Input.GetMouseButtonDown(2)) //middle click
+        {
+            LockOnTriggered();
+        }
+
+        //Speed Modifier
         if (Input.GetAxisRaw("Vertical") == 1)
         {
             moveSpeed *= frontSpeedMultiplier;
         }
-        //Debug.Log("Horizontal:" + horizontal.ToString()+ ",Vertical:" + vertical.ToString());
+        if (Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Vertical") == 0)
+        {
+            moveSpeed *= frontSpeedMultiplier;
+        }
 
+        //animation parameters
         animator.SetFloat("Horizontal", horizontal);
         animator.SetFloat("Vertical", vertical);
         animator.SetFloat("Speed", speed* 0.75f);
         animator.SetBool("Idle", (horizontal == 0 && vertical == 0));
-        //Debug.Log((horizontal == 0 && vertical == 0));
 
-        float targetAngle = /*Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + */camera.eulerAngles.y;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-       // transform.GetChild(0).localRotation = Quaternion.Euler(0f, targetAngle, 0f);
-        transform.localRotation = Quaternion.Euler(0f, angle, 0f);
+        //rotate
+        if (lockOnMode)
+        {
+            Vector3 targetPostition = new Vector3(lockOnTarget.position.x, this.transform.position.y, lockOnTarget.position.z);
+            transform.DOLookAt(targetPostition, turnSmoothTime);
+        }
+        else
+        {
+            //freelook camera
+            float targetAngle = camera.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.localRotation = Quaternion.Euler(0f, angle, 0f);
+        }
 
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector3.up * 3.5f, ForceMode.Impulse);
+        }
+    }
+
+    void LateUpdate()
+    {
+        //move
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= 0.1f)
         {
-            float moveTargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float moveTargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.transform.eulerAngles.y;
             Vector3 moveDirection = Quaternion.Euler(0f, moveTargetAngle, 0f) * Vector3.forward;
-            //controller.Move(moveDirection.normalized * speed * Time.deltaTime);
-            //Vector3 moveVector = new Vector3(0f, moveTargetAngle, 0f) + Vector3.forward;
-            //rigidbody.AddForce(moveDirection * moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
 
             ///CollideCheck Rasycast
             RaycastHit hit;
             RaycastHit hit2;
             Vector3 origin = transform.position;
-            origin += Vector3.up * 0.25f;
+            origin += Vector3.up * climbHeightMaximum;
 
             Vector3 end;
             end = origin + (moveDirection * moveSpeed * 0.15f);
-            Debug.Log("(" + moveDirection.x + "," + moveDirection.y + "," + moveDirection.z + ")");
             if (Physics.Raycast(origin, moveDirection, out hit, moveSpeed * 0.15f, layerMask))
             {
                 Debug.DrawLine(origin, end, Color.red);
@@ -74,16 +118,117 @@ public class ThirdPersonControl : MonoBehaviour
                 }
                 else
                 {
-                    transform.DOBlendableLocalMoveBy(moveDirection * moveSpeed * Time.deltaTime, 0f);
+                    Move(moveDirection);
                     Debug.DrawLine(end, end + Vector3.up * 0.5f, Color.green);
                 }
             }
         }
+    }
 
-        //jump
-        if (Input.GetKeyDown(KeyCode.Space))
+    Transform FindTarget()
+    {
+        Transform rtn = null;
+        GameObject[] enemies;
+        List<GameObject> enemiesInSight = new List<GameObject>();
+        float distanceCompare = lockOnSearchRange;
+
+        //search enemy
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemies != null)
         {
-            rigidbody.AddForce(Vector3.up * 3.5f, ForceMode.Impulse);
+            foreach (GameObject enemy in enemies)
+            {
+                if (IsObjectInSight(enemy.transform, camera))
+                {
+                    enemiesInSight.Add(enemy);
+                }
+            }
         }
+
+        //compare all enemy in sight, get the nearest one
+        if (enemiesInSight != null)
+        {
+            foreach (GameObject enemy in enemiesInSight)
+            {
+                float newDistance = Vector3.Distance(enemy.transform.position, this.transform.position);
+                if (distanceCompare > newDistance)
+                {
+                    distanceCompare = newDistance;
+                    //finally get the target
+                    rtn = enemy.transform;
+                }
+            }
+        }
+
+        //clear everything
+        enemies = null;
+        enemiesInSight.Clear();
+
+        return rtn;
+    }
+
+    private bool IsObjectInSight(Transform transform, Camera cam)
+    {
+        Vector3 viewPos = cam.WorldToViewportPoint(transform.position);
+        if (viewPos.x >= 0 && viewPos.x <= 1
+            && viewPos.y >= 0 && viewPos.y <= 1
+            && viewPos.z > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void LockOnTriggered()
+    {
+        if (!lockOnMode)
+        {
+            //change to lock on mode if the camera can find a target
+            lockOnTarget = FindTarget();
+            if (lockOnTarget != null)
+            {
+                lockOnCamera.Priority = 1;
+                freeLookCamera.Priority = 0;
+                lockOnMode = true;
+
+                
+            }
+        }
+        else
+        {
+            //back to default
+            lockOnCamera.Priority = 0;
+            freeLookCamera.Priority = 1;
+            lockOnTarget = null;
+            lockOnMode = false;
+
+            //reset free look camera's rotation
+        }
+
+        //Canvas
+        Reticle.SetActive(lockOnMode);
+    }
+
+    void Move(Vector3 moveDirection)
+    {
+        //check Y of new position
+        RaycastHit hit;
+        Vector3 origin = transform.position + (moveDirection * moveSpeed * 0.15f);
+        origin += Vector3.up * climbHeightMaximum;
+        bool GroundHit = (Physics.Raycast(origin, Vector3.down, out hit, climbHeightMaximum, layerMask));
+        
+        if (GroundHit && hit.point.y - 0.1f > transform.position.y)
+        {
+            Debug.DrawLine(origin, hit.point, Color.cyan, 2f);
+            //move Y smoothly depends on the length difference
+            transform.DOMoveY(hit.point.y, (hit.point.y - transform.position.y) * YaxisClimbLerpDelay);
+        }
+        else
+        {
+            Debug.DrawLine(origin, origin + new Vector3(0.0f, -climbHeightMaximum, 0.0f), Color.blue);
+        }
+        
+        //move XZ
+        transform.DOBlendableLocalMoveBy(moveDirection * moveSpeed * Time.deltaTime, 0f);
     }
 }
