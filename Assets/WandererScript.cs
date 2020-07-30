d44fd6561c49f451e4cc0d5b0cc85f7af8759828
marybun <phunmung@gmail.com>
2020-07-30 16:42:57 +0900

    dissolve shader

--- Assets/WandererScript.cs
+++ Assets/WandererScript.cs
@@ -10,18 +10,37 @@ public class WandererScript : MonoBehaviour
     private GameObject[] removeAfterDead;
     [Range(0.0f, 10.0f), SerializeField]
     private float delay;
+    [SerializeField]
+    private float control;
+    [SerializeField]
+    private Renderer renderer;
 
     public void Start()
     {
         mainScript.func = Funtion;
+        control = -1.0f;
+        enabled = false;
     }
 
     public void Funtion()
     {
+        enabled = true;
         // this is wanderer specific code that only work for him.
         for (int i = 0; i < removeAfterDead.Length; i++)
         {
             Destroy(removeAfterDead[i], delay);
         }
     }
+
+    public void Update()
+    {
+        control += 0.005f;
+        if (renderer.material.HasProperty("control"))
+        {
+            Debug.Log(control);
+            renderer.material.SetFloat("control" ,control);
+        }
+    }
+
+
 }
